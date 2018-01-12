75b58f83d7811363122ed6f73270b9c29a8adabd
Rockett Sally <dotomasi@outlook.com>
2018-01-12 18:09:35 -0200

    ver 1.0.5 - General Optimization and Fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Platform} from 'ionic-angular';
+import { Platform, App, ToastController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -13,15 +13,49 @@ import { RedditService } from './services/reddit.service';
 })
 export class MyApp {
   rootPage:any = TabsPage;
+  
+	allowClose: any;
+	lastBack: any;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.overlaysWebView(true);
-      statusBar.backgroundColorByHexString("#488aff");
-      splashScreen.hide();
+	constructor(public  app: App, public platform: Platform, public toastCtrl: ToastController, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+		
+		platform.ready().then((readySource) => {
+		  // Okay, so the platform is ready and our plugins are available.
+		  // Here you can do any higher level native things you might need.
+		  statusBar.overlaysWebView(true);
+		  statusBar.backgroundColorByHexString("#488aff");
+		  splashScreen.hide();
+		  
+			let lastTimeBackPress = 0;
+			let timePeriodToExit  = 2500;
+			
+				platform.registerBackButtonAction(() => {
+					const overlay = this.app._appRoot._overlayPortal.getActive();
+					const nav = this.app.getActiveNav();
+					const closeDelay = 2500;
+					const spamDelay = 500;
 
-    });
-  }
+					if(overlay && overlay.dismiss) {
+					  overlay.dismiss();
+					} else if(nav.canGoBack()) {
+					  nav.pop();
+					} else if(Date.now() - this.lastBack > spamDelay && !this.allowClose) {
+					  this.allowClose = true;
+					  let toast = this.toastCtrl.create({
+						message: 'Press Again to Exit App.', //this.translate.instant("general.close_toast"),
+						duration: closeDelay,
+						dismissOnPageChange: true
+					  });
+					  toast.onDidDismiss(() => {
+						this.allowClose = false;
+					  });
+					  toast.present();
+					} else if(Date.now() - this.lastBack < closeDelay && this.allowClose) {
+					  this.platform.exitApp();
+					}
+					this.lastBack = Date.now();
+				});
+
+		});
+	}
 }
